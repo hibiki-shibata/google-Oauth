caf1cad69625360447be5e07344b9070a7415fa7
Hibiki Shibata <hibikishibata1853@gmail.com>
2024-05-12 23:51:58 +0900

    dependencies' version upgrade

--- googleOauth.py
+++ googleOauth.py
@@ -1,19 +1,17 @@
 import os
 import pathlib
 
-import requests
 from flask import Flask, session, abort, redirect, request
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
-from pip._vendor import cachecontrol
 import google.auth.transport.requests
 
 app = Flask("Google Login App")
-app.secret_key = "<SECRET_KEY>" # make sure this matches with that's in client_secret.json
+app.secret_key = "<replace_later>" # make sure this matches with that's in client_secret.json
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1" # to allow Http traffic for local dev
 
-GOOGLE_CLIENT_ID = "<>.apps.googleusercontent.com"
+GOOGLE_CLIENT_ID = "<replace_later>"
 client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
 
 flow = Flow.from_client_secrets_file(
@@ -36,21 +34,25 @@ def login_is_required(function):
 @app.route("/login")
 def login():
     authorization_url, state = flow.authorization_url()
-    session["state"] = state
+    session["state"] = state # to validate if the person accessed to /callback is the person who started login
     return redirect(authorization_url)
 
 
 @app.route("/callback")
 def callback():
     flow.fetch_token(authorization_response=request.url)
+     # called back URI includes the Oauth token
 
     if not session["state"] == request.args["state"]:
         abort(500)  # State does not match!
 
+# Flow will generate credentials for requests to google server based on the fetched token 
     credentials = flow.credentials
-    request_session = requests.session()
-    cached_session = cachecontrol.CacheControl(request_session)
-    token_request = google.auth.transport.requests.Request(session=cached_session)
+    
+    # request_session = requests.session()
+    # cached_session = cachecontrol.CacheControl(request_session)
+    # token_request = google.auth.transport.requests.Request(session=cached_session)
+    token_request = google.auth.transport.requests.Request()
 
     id_info = id_token.verify_oauth2_token(
         id_token=credentials._id_token,
@@ -61,6 +63,7 @@ def callback():
     session["google_id"] = id_info.get("sub")
     session["name"] = id_info.get("name")
     session["email"] = id_info.get("email")
+    
     return redirect("/protected_area")
 
 
